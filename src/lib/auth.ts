f1961a97174d8c4916d6fd4e2dfe77cfb1015a6d
onikarubi <printuserselect@gmail.com>
2025-03-23 17:12:16 +0900

    init commit.

---
+++ src/lib/auth.ts
@@ -0,0 +1,32 @@
+import NextAuth, { NextAuthConfig } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+const GOOGLE_AUTHORIZATION_URL =
+  'https://accounts.google.com/o/oauth2/v2/auth?' +
+  new URLSearchParams({
+    prompt: 'consent',
+    access_type: 'offline',
+    response_type: 'code',
+  });
+
+const nextAuthConfig: NextAuthConfig = {
+  trustHost: true,
+  secret: process.env.NEXTAUTH_SECRET || 'next-auth-secret',
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+      authorization: GOOGLE_AUTHORIZATION_URL,
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+};
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth(nextAuthConfig);
